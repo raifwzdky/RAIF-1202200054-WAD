9ff5ed208b586c26873ee53c9bd6ffd76d95dd8c
raifwzdky <fawwazdzaky12@gmail.com>
2023-07-15 13:46:55 +0700

    done

---
+++ Pagelanding.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WAD TEST KEMAMPUAN SOFTWARE 2023</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary px-3">
+  <div class="container-fluid">
+    <!-- logo -->
+    <a class="navbar-brand" href="#">
+      <img src="assets/LOGO.png" alt="Logo" height="25px" class="d-inline-block align-text-top">
+    </a>
+    <!--  -->
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav me-auto justify-content-lg-end" style="">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Service</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Testimoni</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+        <button class="btn btn-outline-secondary" style="width:100px">Login</button>
+        <button class="btn btn-primary mx-2" style="width:100px">Register</button>
+    </div>
+  </div>
+</nav>
+<!-- Navbar End -->
+
+<!-- Home -->
+<section>
+  <div class="container-x1 mt-5">
+    <div class="row d-flex align-items-center">
+      <div class="col-lg-6 ps-5">
+        <div class="col-6">
+
+          <h1 class="fw-bold">
+            Unleash Your Style, Rent Your Fashion.
+          </h1>
+        </div>
+        <div class="col-6">
+          <p>Welcome to our Fashion Rental platform, where style knows no boundaries. Discover a world of endless possibilities as you explore.</p>
+        </div>
+        <div class="d-md-flex justify-content-md-start">
+          <button type="button" class="btn btn-info btn-lg px-4 me-md-2 text-white opacity-50">Get Started </button>
+          <button type="button" class="btn btn-lg px-4 bg-transparent text-secondary text-center"><img src="assets/play-button.png" class="opacity-50" style="height:30px">Watch Trailer</button>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+          <img src="assets/pic1.png" class="d-block mx-lg-auto img-fluid" width="400">
+      </div>
+    </div>
+  </div>
+</section>
+<!-- end home -->
+
+<!-- service -->
+    <section>
+        <div class="px-4 pt-5 my-5 text-center ">
+            <h2 class="display-4 fw-bold text-body-emphasis">Our Services</h2>
+            <div class="col-lg-4 mx-auto">
+              <p class="lead text-secondary">Welcome to our Fashion Rental Platform, where style knows no boundaries.</p>
+            </div>
+        </div>
+        <div class="container-xl">
+            <div class="row">
+                <div class="col-4">
+                    <div class="card border-0" style="width:400px">
+                        <img src="assets/service1.png" class="card-img-top" alt="" style="width:80%">
+                        <div class="card-body">
+                        <h4 class="card-title">Services 1</h4>
+                        <p class="card-text">Welcome to our Fashion Rental Platform, where style knows no boundaries.</p>
+                        <h3 class="" style="color:#FF7EC4;">Start From $123</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="col-4">
+                        <div class="card border-0" style="width:400px">
+                            <img src="assets/service2.png" alt="" class="card-img-top" style="width:80%">
+                            <div class="card-body">
+                            <h4 class="card-title">Services 2</h4>
+                            <p class="card-text">Welcome to our Fashion Rental Platform, where style knows no boundaries.</p>
+                            <h3 class="" style="color:#FF7EC4;">Start From $123</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="col-4">
+                        <div class="card border-0" style="width:400px">
+                            <img src="assets/service3.png" alt="" class="card-img-top" style="width:80%">
+                            <div class="card-body">
+                            <h4 class="card-title">Services 3</h4>
+                            <p class="card-text">Welcome to our Fashion Rental Platform, where style knows no boundaries.</p>
+                            <h3 class="" style="color:#FF7EC4;">Start From $123</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<!-- service end -->
+
+<!-- testimoni -->
+  <section>
+    <div class="px-4 pt-5 my-5 text-center">
+      <h2 class="display-4 fw-bold text-body-emphasis">Testimoni</h2>
+        <div class="col-lg-4 mx-auto">
+        <p class="lead text-secondary">Welcome to our Fashion Rental Platform, where style knows no boundaries.</p>
+        </div>
+    </div>
+
+      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="d-flex justify-content-center">
+            <div class="col-lg-4">
+              <div class="carousel-item active">
+                <img src="assets/jarwo.png" class="d-block w-100" alt="...">
+                <h2 class="fw-normal mt-2">Jarwo Kuat
+                  <span><img src="assets/star.png" style="width:25px" alt="">4.5</span>
+                </h2>
+                <p class=" text-secondary">Welcome to our Fashion Rental Platform, where style knows no boundaries.</p>
+              </div>
+              <div class="carousel-item">
+                <img src="assets/jarwo.png" class="d-block w-100" alt="...">
+                <h2 class="fw-normal mt-2">Jarwo Kuat
+                  <span><img src="assets/star.png" style="width:25px" alt="">4.5</span>
+                </h2>
+                <p class=" text-secondary">Welcome to our Fashion Rental Platform, where style knows no boundaries.</p>
+              </div>
+            </div>
+          </div>          
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+  </div>
+  </section>
+<!-- testimoni end -->
+
+<!-- contact us -->
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-6">
+      <div class="pt-4">
+        <h2 class="fw-bold text-body">Contact US</h2>
+        <div class="col-8">
+          <p class="text-secondary fw-semibold">Welcome to our Fashion Rental Platform, where style knows no boundaries.</p>
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Email</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+
+    <div class="col-6">
+    <div class="mapouter">
+      <div class="gmap_canvas">
+        <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=telkom university&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+        </iframe>
+        <a href="https://connectionsgame.org/">Connections Game</a>
+      </div>
+        <style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}
+        </style>
+      </div>
+      <div class="sosmed">
+        <img src="assets/whatsapp.png" class="mt-3" style="width:50px" alt="">
+        <img src="assets/instagram.png" class="mt-3" style="width:50px" alt="">
+        <img src="assets/linkedin.png" class="mt-3" style="width:50px" alt="">
+        <img src="assets/facebook.png" class="mt-3" style="width:50px" alt="">
+      </div>
+      
+    </div>
+  </div>
+</div>
+<!-- contact us end -->
+
+<!-- footer -->
+<section>
+  <footer class="mt-5">
+    <div class="bg-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-9">
+            <h1 class="text-white" style="margin-top:75px">
+              WAD2023
+            </h1>
+          </div>
+          <div class="col-3">
+            <div class="text-white mt-5">
+              <p>Nama : Raif Fawwazdzaky</p>
+              <p>Nim  : 1202200054</p>
+              <p>Kode Asprak : RAEP</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</section>
+<!-- footer emd -->
+
+</body>
+</html>
